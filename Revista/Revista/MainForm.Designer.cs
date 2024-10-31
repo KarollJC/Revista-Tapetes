7a522c649830faafef855fdec0329bf2f1047279
Minicapinon <bava080502hgtrtna6@cbtis217.edu.mx>
2024-10-31 10:33:26 +0100

    Avanze código

--- Revista/Revista/MainForm.Designer.cs
+++ Revista/Revista/MainForm.Designer.cs
@@ -37,79 +37,85 @@ namespace Revista
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.pictureBox3 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
+			this.picboxtapeteder = new System.Windows.Forms.PictureBox();
+			this.picboxtapeteizq = new System.Windows.Forms.PictureBox();
+			this.lbltapetecen = new System.Windows.Forms.Label();
+			this.lbltapeteder = new System.Windows.Forms.Label();
+			this.lbltapeteizq = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
-			this.pictureBox4 = new System.Windows.Forms.PictureBox();
 			this.pictureBox7 = new System.Windows.Forms.PictureBox();
 			this.pictureBox8 = new System.Windows.Forms.PictureBox();
 			this.pictureBox9 = new System.Windows.Forms.PictureBox();
 			this.pictureBox10 = new System.Windows.Forms.PictureBox();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.picboxtapetecen = new System.Windows.Forms.PictureBox();
+			this.pblikr = new System.Windows.Forms.PictureBox();
+			this.pbant = new System.Windows.Forms.PictureBox();
+			this.pbnext = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.bntencu = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.picboxtapeteder)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picboxtapeteizq)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picboxtapetecen)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pblikr)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbant)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbnext)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// pictureBox2
+			// picboxtapeteder
 			// 
-			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-			this.pictureBox2.Location = new System.Drawing.Point(430, 156);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(121, 144);
-			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox2.TabIndex = 1;
-			this.pictureBox2.TabStop = false;
+			this.picboxtapeteder.Location = new System.Drawing.Point(430, 156);
+			this.picboxtapeteder.Name = "picboxtapeteder";
+			this.picboxtapeteder.Size = new System.Drawing.Size(121, 144);
+			this.picboxtapeteder.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picboxtapeteder.TabIndex = 1;
+			this.picboxtapeteder.TabStop = false;
 			// 
-			// pictureBox3
+			// picboxtapeteizq
 			// 
-			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-			this.pictureBox3.Location = new System.Drawing.Point(69, 156);
-			this.pictureBox3.Name = "pictureBox3";
-			this.pictureBox3.Size = new System.Drawing.Size(121, 144);
-			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox3.TabIndex = 2;
-			this.pictureBox3.TabStop = false;
+			this.picboxtapeteizq.Location = new System.Drawing.Point(69, 156);
+			this.picboxtapeteizq.Name = "picboxtapeteizq";
+			this.picboxtapeteizq.Size = new System.Drawing.Size(121, 144);
+			this.picboxtapeteizq.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picboxtapeteizq.TabIndex = 2;
+			this.picboxtapeteizq.TabStop = false;
 			// 
-			// label1
+			// lbltapetecen
 			// 
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(230, 329);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(164, 23);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "Titulo del tapete";
+			this.lbltapetecen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbltapetecen.Location = new System.Drawing.Point(230, 329);
+			this.lbltapetecen.Name = "lbltapetecen";
+			this.lbltapetecen.Size = new System.Drawing.Size(164, 23);
+			this.lbltapetecen.TabIndex = 3;
+			this.lbltapetecen.Text = "Titulo del tapete";
 			// 
-			// label2
+			// lbltapeteder
 			// 
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(430, 303);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(121, 23);
-			this.label2.TabIndex = 4;
-			this.label2.Text = "Titulo del tapete";
+			this.lbltapeteder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbltapeteder.Location = new System.Drawing.Point(430, 303);
+			this.lbltapeteder.Name = "lbltapeteder";
+			this.lbltapeteder.Size = new System.Drawing.Size(121, 23);
+			this.lbltapeteder.TabIndex = 4;
+			this.lbltapeteder.Text = "Titulo del tapete";
 			// 
-			// label3
+			// lbltapeteizq
 			// 
-			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(69, 303);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(121, 23);
-			this.label3.TabIndex = 5;
-			this.label3.Text = "Titulo del tapete";
+			this.lbltapeteizq.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbltapeteizq.Location = new System.Drawing.Point(69, 303);
+			this.lbltapeteizq.Name = "lbltapeteizq";
+			this.lbltapeteizq.Size = new System.Drawing.Size(121, 23);
+			this.lbltapeteizq.TabIndex = 5;
+			this.lbltapeteizq.Text = "Titulo del tapete";
 			// 
 			// button1
 			// 
 			this.button1.BackColor = System.Drawing.Color.White;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
 			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button1.Location = new System.Drawing.Point(262, 366);
 			this.button1.Name = "button1";
@@ -121,23 +127,14 @@ namespace Revista
 			// button2
 			// 
 			this.button2.BackColor = System.Drawing.Color.White;
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
 			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button2.Location = new System.Drawing.Point(69, 422);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(93, 28);
 			this.button2.TabIndex = 7;
-			this.button2.Text = "Traducir";
+			this.button2.Text = "Translate";
 			this.button2.UseVisualStyleBackColor = false;
-			// 
-			// pictureBox4
-			// 
-			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-			this.pictureBox4.Location = new System.Drawing.Point(-2, 182);
-			this.pictureBox4.Name = "pictureBox4";
-			this.pictureBox4.Size = new System.Drawing.Size(321, 326);
-			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox4.TabIndex = 12;
-			this.pictureBox4.TabStop = false;
 			// 
 			// pictureBox7
 			// 
@@ -180,15 +177,74 @@ namespace Revista
 			this.pictureBox10.TabIndex = 11;
 			this.pictureBox10.TabStop = false;
 			// 
-			// pictureBox1
+			// picboxtapetecen
 			// 
-			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(230, 142);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(159, 184);
-			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
+			this.picboxtapetecen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.picboxtapetecen.Location = new System.Drawing.Point(230, 142);
+			this.picboxtapetecen.Name = "picboxtapetecen";
+			this.picboxtapetecen.Size = new System.Drawing.Size(159, 184);
+			this.picboxtapetecen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picboxtapetecen.TabIndex = 0;
+			this.picboxtapetecen.TabStop = false;
+			// 
+			// pblikr
+			// 
+			this.pblikr.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pblikr.BackgroundImage")));
+			this.pblikr.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pblikr.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pblikr.Location = new System.Drawing.Point(390, 335);
+			this.pblikr.Name = "pblikr";
+			this.pblikr.Size = new System.Drawing.Size(40, 40);
+			this.pblikr.TabIndex = 15;
+			this.pblikr.TabStop = false;
+			this.pblikr.Click += new System.EventHandler(this.PblikrClick);
+			// 
+			// pbant
+			// 
+			this.pbant.Image = ((System.Drawing.Image)(resources.GetObject("pbant.Image")));
+			this.pbant.Location = new System.Drawing.Point(-2, 182);
+			this.pbant.Name = "pbant";
+			this.pbant.Size = new System.Drawing.Size(321, 326);
+			this.pbant.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbant.TabIndex = 12;
+			this.pbant.TabStop = false;
+			// 
+			// pbnext
+			// 
+			this.pbnext.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbnext.BackgroundImage")));
+			this.pbnext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pbnext.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pbnext.Location = new System.Drawing.Point(570, 221);
+			this.pbnext.Name = "pbnext";
+			this.pbnext.Size = new System.Drawing.Size(40, 40);
+			this.pbnext.TabIndex = 16;
+			this.pbnext.TabStop = false;
+			this.pbnext.Click += new System.EventHandler(this.PbnextClick);
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+			this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox6.Location = new System.Drawing.Point(12, 221);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(40, 40);
+			this.pictureBox6.TabIndex = 17;
+			this.pictureBox6.TabStop = false;
+			this.pictureBox6.Click += new System.EventHandler(this.PictureBox6Click);
+			// 
+			// bntencu
+			// 
+			this.bntencu.BackColor = System.Drawing.Color.White;
+			this.bntencu.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.bntencu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.bntencu.Location = new System.Drawing.Point(520, 360);
+			this.bntencu.Name = "bntencu";
+			this.bntencu.Size = new System.Drawing.Size(93, 28);
+			this.bntencu.TabIndex = 18;
+			this.bntencu.Text = "Encuesta";
+			this.bntencu.UseVisualStyleBackColor = false;
+			this.bntencu.Click += new System.EventHandler(this.BntencuClick);
 			// 
 			// MainForm
 			// 
@@ -196,43 +252,57 @@ namespace Revista
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.White;
 			this.ClientSize = new System.Drawing.Size(622, 481);
+			this.Controls.Add(this.bntencu);
+			this.Controls.Add(this.pictureBox6);
+			this.Controls.Add(this.pbnext);
+			this.Controls.Add(this.pblikr);
 			this.Controls.Add(this.pictureBox8);
 			this.Controls.Add(this.pictureBox7);
 			this.Controls.Add(this.pictureBox10);
 			this.Controls.Add(this.pictureBox9);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.pictureBox3);
-			this.Controls.Add(this.pictureBox2);
-			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.lbltapeteizq);
+			this.Controls.Add(this.lbltapeteder);
+			this.Controls.Add(this.lbltapetecen);
+			this.Controls.Add(this.picboxtapeteizq);
+			this.Controls.Add(this.picboxtapeteder);
+			this.Controls.Add(this.picboxtapetecen);
+			this.Controls.Add(this.pbant);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximumSize = new System.Drawing.Size(638, 520);
+			this.MinimumSize = new System.Drawing.Size(638, 520);
 			this.Name = "MainForm";
 			this.Text = "Revista";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picboxtapeteder)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picboxtapeteizq)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picboxtapetecen)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pblikr)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbant)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbnext)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Button bntencu;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.PictureBox pbnext;
+		private System.Windows.Forms.PictureBox pblikr;
 		private System.Windows.Forms.PictureBox pictureBox10;
 		private System.Windows.Forms.PictureBox pictureBox9;
 		private System.Windows.Forms.PictureBox pictureBox8;
 		private System.Windows.Forms.PictureBox pictureBox7;
-		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pbant;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.PictureBox pictureBox3;
-		private System.Windows.Forms.PictureBox pictureBox2;
-		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label lbltapeteizq;
+		private System.Windows.Forms.Label lbltapeteder;
+		private System.Windows.Forms.Label lbltapetecen;
+		private System.Windows.Forms.PictureBox picboxtapeteizq;
+		private System.Windows.Forms.PictureBox picboxtapeteder;
+		private System.Windows.Forms.PictureBox picboxtapetecen;
 	}
 }
--- Revista/Revista/MainForm.cs
+++ Revista/Revista/MainForm.cs
@@ -1,33 +1,170 @@
-﻿/*
- * Created by SharpDevelop.
- * User: CC1_PC35
- * Date: 25/10/2024
- * Time: 08:12 a. m.
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Revista
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
-		}
-	}
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public int lugar_centr = 2;
+        private bool liked = false;
+
+        private void PbnextClick(object sender, EventArgs e)
+        {
+            if (lugar_centr == 10)
+            {
+                lugar_centr = 1;
+            }
+            else
+            {
+                lugar_centr++;
+            }
+
+            UpdatePictureBoxes();
+            UpdateName();
+        }
+
+        private void PictureBox6Click(object sender, EventArgs e)
+        {
+            if (lugar_centr == 1)
+            {
+                lugar_centr = 10;
+            }
+            else
+            {
+                lugar_centr--;
+            }
+
+            UpdatePictureBoxes();
+            UpdateName();
+        }
+
+        private void PblikrClick(object sender, EventArgs e)
+        {
+            if (!liked)
+            {
+                butonlaik();
+                liked = true;
+            }
+            else
+            {
+                butonlaik();
+                liked = false;
+            }
+        }
+
+        private void BntencuClick(object sender, EventArgs e)
+        {
+            string url = "https://forms.gle/4TRpoqRZaU7RE5416";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void butonlaik()
+        {
+            string basePath = @"C:\Users\CC1_PC47\Documents\Revista-Tapetes\Revista\Images\";
+            string imageName = liked ? "like.png" : "likegave.png";
+            string location = Path.Combine(basePath, imageName);
+            pblikr.Image = null;
+            pblikr.ImageLocation = location;
+            pblikr.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void UpdatePictureBoxes()
+        {
+            string centro;
+            string left;
+            string right;
+
+            switch (lugar_centr)
+            {
+                case 1:
+                    left = "tapete10";
+                    centro = "tapete1";
+                    right = "tapete2";
+                    break;
+                case 2:
+                    left = "tapete1";
+                    centro = "tapete2";
+                    right = "tapete3";
+                    break;
+                case 3:
+                    left = "tapete2";
+                    centro = "tapete3";
+                    right = "tapete4";
+                    break;
+                case 4:
+                    left = "tapete3";
+                    centro = "tapete4";
+                    right = "tapete5";
+                    break;
+                case 5:
+                    left = "tapete4";
+                    centro = "tapete5";
+                    right = "tapete6";
+                    break;
+                case 6:
+                    left = "tapete5";
+                    centro = "tapete6";
+                    right = "tapete7";
+                    break;
+                case 7:
+                    left = "tapete6";
+                    centro = "tapete7";
+                    right = "tapete8";
+                    break;
+                case 8:
+                    left = "tapete7";
+                    centro = "tapete8";
+                    right = "tapete9";
+                    break;
+                case 9:
+                    left = "tapete8";
+                    centro = "tapete9";
+                    right = "tapete10";
+                    break;
+                case 10:
+                    left = "tapete9";
+                    centro = "tapete10";
+                    right = "tapete1";
+                    break;
+                default:
+                    left = centro = right = "";
+                    break;
+            }
+
+            string basePath = @"C:\Users\CC1_PC47\Documents\Revista-Tapetes\Revista\Images\";
+            picboxtapeteizq.ImageLocation = Path.Combine(basePath, left);
+            picboxtapeteizq.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            picboxtapetecen.ImageLocation = Path.Combine(basePath, centro);
+            picboxtapetecen.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            picboxtapeteder.ImageLocation = Path.Combine(basePath, right);
+            picboxtapeteder.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        
+        void UpdateName()
+        {
+        	string tap1;
+            string tap2;
+            string tap3;
+        	switch (lugar_centr)
+        	{
+        		case 1:
+        			
+        			break;
+        		default:
+        			
+        			break;
+        	}
+        }
+    }
 }
